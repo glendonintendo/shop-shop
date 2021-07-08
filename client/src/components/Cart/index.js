211100996f09951a444263eec0a1ad467db9f885
Glen Eggers <gleneggers64@gmail.com>
2021-07-08 11:59:09 -0500

    add redux global state management for cart

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -3,29 +3,32 @@ import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 
-import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import { idbPromise } from '../../utils/helpers';
 import { QUERY_CHECKOUT } from '../../utils/queries';
 import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/client';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { addedMultipleToCart, toggledCart } from '../../features/cartSlice';
+
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-	const [state, dispatch] = useStoreContext();
+  const { cart, cartOpen } = useSelector(state => state.cart);
 	const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+  const dispatch = useDispatch();
 
 	useEffect(() => {
 		async function getCart() {
 			const cart = await idbPromise('cart', 'get');
-			dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+			dispatch(addedMultipleToCart([...cart]));
 		};
 
-		if (!state.cart.length) {
+		if (!cart.length) {
 			getCart();
 		}
-	}, [state.cart.length, dispatch]);
+	}, [cart.length, dispatch]);
 
 	useEffect(() => {
 		if (data) {
@@ -35,20 +38,19 @@ const Cart = () => {
 		}
 	}, [data]);
 
-	function toggleCart() {
-		dispatch({ type: TOGGLE_CART });
-	};
-
+  function toggleCart() {
+    dispatch(toggledCart());
+  }
 	function calculateTotal() {
 		let sum = 0;
-		state.cart.forEach(item => sum += item.price * item.purchaseQuantity)
+		cart.forEach(item => sum += item.price * item.purchaseQuantity)
 		return sum.toFixed(2);
 	};
 
 	function submitCheckout() {
 		const productIds = [];
 
-		state.cart.forEach(item => {
+		cart.forEach(item => {
 			for (let i = 0; i < item.purchaseQuantity; i++) {
 				productIds.push(item._id);
 			}
@@ -59,7 +61,7 @@ const Cart = () => {
 		});
 	};
 
-	if (!state.cartOpen) {
+	if (!cartOpen) {
 		return (
 			<div className='cart-closed' onClick={toggleCart}>
 				<span
@@ -74,9 +76,9 @@ const Cart = () => {
 		<div className='cart'>
 			<div className='close' onClick={toggleCart}>[close]</div>
 			<h2>Shopping Cart</h2>
-			{state.cart.length ? (
+			{cart.length ? (
 				<div>
-					{state.cart.map(item => (
+					{cart.map(item => (
 						<CartItem key={item._id} item={item} />
 					))}
 					<div className ='flex-row space-between'>
